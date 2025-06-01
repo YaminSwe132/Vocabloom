bc8a734aafeaab807a768d59b87de933588f4dfa
Lo3y <62650848+Lo3y@users.noreply.github.com>
2025-06-01 14:14:55 +0630

    Vocabloom template

---
+++ VocabloomApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index2.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+
+def course(request):
+    return render(request, 'course.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blog_single(request):
+    return render(request, 'blog_single.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
